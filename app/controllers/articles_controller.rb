3a90f3c29049d0233430eaee6fcf1f1dcdc2a9be
Ubuntu <vagrant@Base-Box>
2019-01-20 21:28:14 +0000

    Added validation, and added the update part of CRUD.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,15 +3,23 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
-    
+
     def new  
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
        @article = Article.new(article_params)
 
-       @article.save 
-       redirect_to @article
+       if @article.save 
+        redirect_to @article
+       else 
+        render 'new'
+       end
     end
 
     def show
